35e72ec33d51e6c8caf614fb165fffdffa6a3945
Terry Rogers <terryrogers@outlook.com>
2014-05-28 13:03:24 -0700

    Added the Cowbell. It's amaze balls.

---
+++ EpochEscape/Assets/Scripts/Cowbell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cowbell : MonoBehaviour
+{
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			CameraBehavior cameraBehavior = Camera.main.GetComponent<CameraBehavior>();
+
+			if(cameraBehavior != null)
+				cameraBehavior.m_displayCowbell = true;
+
+			Destroy(gameObject);
+		}
+	}
+}
