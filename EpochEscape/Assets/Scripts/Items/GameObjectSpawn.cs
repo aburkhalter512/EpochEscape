3fe1b520e8f0d657147aad1a398d2f4f28154194
aburkhalter512 <aburkhalter512@gmail.com>
2014-06-07 16:59:32 -0700

    Added the GameObjectSpawner and the SpecialItemSpawner.

---
+++ EpochEscape/Assets/Scripts/Items/GameObjectSpawn.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectSpawner : MonoBehaviour
+{
+	#region Inspector Variables
+    public GameObject item = null;
+	#endregion
+
+	#region Instance Variables
+	#endregion
+
+	#region Class Constants
+	#endregion
+
+	//Put all initialization code here
+	//Remember to comment!
+	protected void Start()
+	{
+        if (item != null)
+        {
+            GameObject toPlace = GameObject.Instantiate(item) as GameObject;
+            toPlace.transform.position = transform.position;
+        }
+
+        Destroy(gameObject);
+	}
+
+	#region Initialization Methods
+	#endregion
+
+	//Put all update code here
+	//Remember to comment!
+	protected void Update()
+	{
+	}
+
+	#region Update Methods
+	#endregion
+
+	#region Static Methods
+	#endregion
+
+	#region Utilities
+	#endregion
+}
---
+++ EpochEscape/Assets/Scripts/Items/SpecialItemSpawn.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using G = GameManager;
+
+public class SpecialItemSpawn : MonoBehaviour
+{
+	#region Inspector Variables
+	#endregion
+
+	#region Instance Variables
+	#endregion
+
+	#region Class Constants
+	#endregion
+
+	//Put all initialization code here
+	//Remember to comment!
+	protected void Start()
+	{
+        switch (G.getInstance().m_currentCharacter)
+        {
+            case G.CAVEGIRL:
+                Club club = Resources.Load<Club>("Prefabs/Items/SpecialItems/Club");
+                club.gameObject.transform.position = transform.position;
+                break;
+            case G.KNIGHT:
+                Shield club = Resources.Load<Club>("Prefabs/Items/SpecialItems/Club");
+                club.gameObject.transform.position = transform.position;
+                break;
+            case G.MUMMY:
+                break;
+            case G.NINJA:
+                break;
+            case G.ASTRONAUT:
+                break;
+            case G.ROBOT:
+                break;
+        }
+
+        Destroy(gameObject);
+	}
+
+	#region Initialization Methods
+	#endregion
+
+	//Put all update code here
+	//Remember to comment!
+	protected void Update()
+	{
+	}
+
+	#region Update Methods
+	#endregion
+
+	#region Static Methods
+	#endregion
+
+	#region Utilities
+	#endregion
+}
