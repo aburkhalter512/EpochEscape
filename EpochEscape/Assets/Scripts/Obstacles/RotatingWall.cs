6c2a3283a138f7923aaa286fcc2e0135ce67cf66
Terry Rogers <terryrogers@outlook.com>
2014-06-08 23:28:44 -0700

    Cameras on rotating walls has been implemented.

--- EpochEscape/Assets/Scripts/Obstacles/RotatingWall.cs
+++ EpochEscape/Assets/Scripts/Obstacles/RotatingWall.cs
@@ -3,121 +3,121 @@ using System.Collections;
 
 public class RotatingWall : DynamicWall
 {
-    #region Inspector Variables
-    public ROTATION_POINTS rotationPt;
-    public float[] rotationAngles;
-
-    public Vector3 customRotationPoint;
+	#region Inspector Variables
+	public ROTATION_POINTS rotationPt;
+	public float[] rotationAngles;
+	
+	public Vector3 customRotationPoint;
 	#endregion
-
-    #region Instance Variables
-    private Vector3 realRotationPt = Vector3.zero;
-    private float baseAngle = 0.0f;
-    private float originalAngle = 0.0f;
-    private float destinationAngle = 0.0f;
-
-    private float currentRotationChange = 0.0f;
+	
+	#region Instance Variables
+	private Vector3 realRotationPt = Vector3.zero;
+	private float baseAngle = 0.0f;
+	private float originalAngle = 0.0f;
+	private float destinationAngle = 0.0f;
+	
+	private float currentRotationChange = 0.0f;
 	#endregion
-
+	
 	#region Class Constants
-    public enum ROTATION_POINTS
-    {
-        CENTER = 0,
-        TOP_RIGHT,
-        BOTTOM_RIGHT,
-        BOTTOM_LEFT,
-        TOP_LEFT,
-        CUSTOM
-    };
+	public enum ROTATION_POINTS
+	{
+		CENTER = 0,
+		TOP_RIGHT,
+		BOTTOM_RIGHT,
+		BOTTOM_LEFT,
+		TOP_LEFT,
+		CUSTOM
+	};
 	#endregion
-
+	
 	//Put all initialization code here
 	//Remember to comment!
 	protected new void Start()
 	{
-        base.Start();
-
-        switch (rotationPt)
-        {
-            case ROTATION_POINTS.CENTER:
-                break;
-            case ROTATION_POINTS.TOP_RIGHT:
-                realRotationPt.x = size.x;
-                realRotationPt.y = size.y;
-                break;
-            case ROTATION_POINTS.BOTTOM_RIGHT:
-                realRotationPt.x = size.x;
-                realRotationPt.y = -size.y;
-                break;
-            case ROTATION_POINTS.BOTTOM_LEFT:
-                realRotationPt.x = -size.x;
-                realRotationPt.y = -size.y;
-                break;
-            case ROTATION_POINTS.TOP_LEFT:
-                realRotationPt.x = -size.x;
-                realRotationPt.y = size.y;
-                break;
-            case ROTATION_POINTS.CUSTOM:
-                realRotationPt = customRotationPoint;
-                break;
-        }
-
-        realRotationPt += transform.position;
-
-        baseAngle = transform.rotation.eulerAngles.z;
-
-        for (int i = 0; i < rotationAngles.Length; i++)
-            rotationAngles[i] %= 360;
+		base.Start();
+		
+		switch (rotationPt)
+		{
+		case ROTATION_POINTS.CENTER:
+			break;
+		case ROTATION_POINTS.TOP_RIGHT:
+			realRotationPt.x = size.x;
+			realRotationPt.y = size.y;
+			break;
+		case ROTATION_POINTS.BOTTOM_RIGHT:
+			realRotationPt.x = size.x;
+			realRotationPt.y = -size.y;
+			break;
+		case ROTATION_POINTS.BOTTOM_LEFT:
+			realRotationPt.x = -size.x;
+			realRotationPt.y = -size.y;
+			break;
+		case ROTATION_POINTS.TOP_LEFT:
+			realRotationPt.x = -size.x;
+			realRotationPt.y = size.y;
+			break;
+		case ROTATION_POINTS.CUSTOM:
+			realRotationPt = customRotationPoint;
+			break;
+		}
+		
+		realRotationPt += transform.position;
+		
+		baseAngle = transform.rotation.eulerAngles.z;
+		
+		for (int i = 0; i < rotationAngles.Length; i++)
+			rotationAngles[i] %= 360;
 	}
-
+	
 	#region Initialization Methods
 	#endregion
-
+	
 	//Put all update code here
 	//Remember to comment!
 	protected new void Update()
 	{
-        base.Update();
+		base.Update();
 	}
-
-    #region Update Methods
-    protected override void stationary()
-    {
-        //Empty Method
-    }
-
-    protected override void toChange()
-    {
+	
+	#region Update Methods
+	protected override void stationary()
+	{
+		//Empty Method
+	}
+	
+	protected override void toChange()
+	{
 		audio.Play ();
-        currentIndex = (currentIndex + 1) % rotationAngles.Length;
-
-        destinationAngle = rotationAngles[currentIndex] - transform.eulerAngles.z;
-
-        if (destinationAngle > 180)
-            destinationAngle = destinationAngle - 360;
-        else if (destinationAngle < -180)
-            destinationAngle = destinationAngle + 360;
-
-        /*if (destinationAngle < -180)
-            destinationAngle += 360;*/
-
-        originalAngle = transform.eulerAngles.z;
-
-        currentRotationChange = destinationAngle / changeTime;
-
-        UpdateSize();
-
-        Debug.Log(destinationAngle);
-
+		currentIndex = (currentIndex + 1) % rotationAngles.Length;
+		
+		destinationAngle = rotationAngles[currentIndex] - transform.eulerAngles.z;
+		
+		if (destinationAngle > 180)
+			destinationAngle = destinationAngle - 360;
+		else if (destinationAngle < -180)
+			destinationAngle = destinationAngle + 360;
+		
+		/*if (destinationAngle < -180)
+			destinationAngle += 360;*/
+		
+		originalAngle = transform.eulerAngles.z;
+		
+		currentRotationChange = destinationAngle / changeTime;
+		
+		UpdateSize();
+		
+		//Debug.Log(destinationAngle);
+		
 		currentState = STATES.CHANGE;
-    }
-
-    protected override void change()
-    {
-        if (Mathf.Approximately(currentRotationChange, 0.0f))
-        {
-            currentState = STATES.STATIONARY;
-
+	}
+	
+	protected override void change()
+	{
+		if (Mathf.Approximately(currentRotationChange, 0.0f))
+		{
+			currentState = STATES.STATIONARY;
+			
 			// ---
 			// This block was originally inside the stationary() method, but for some reason it wouldn't work.
 			CameraBehavior cameraBehavior = Camera.main.GetComponent<CameraBehavior>();
@@ -125,36 +125,53 @@ public class RotatingWall : DynamicWall
 			if(cameraBehavior.m_currentState == CameraBehavior.State.LERP_REST)
 				cameraBehavior.m_currentState = CameraBehavior.State.LERP_TO_TARGET;
 			// --- //*/
-
-            return;
-        }
 			
+			for(int i = 0; i < transform.childCount; i++)
+			{
+				if(transform.GetChild(i).tag == "SecurityCamera")
+				{
+					SecurityCamera cam = transform.GetChild(i).GetComponent<SecurityCamera>();
+
+					float sn = Mathf.Sin(destinationAngle * Mathf.Deg2Rad);
+					float cs = Mathf.Cos(destinationAngle * Mathf.Deg2Rad);
+
+					float px = cam.m_resetDirection.x * cs - cam.m_resetDirection.y * sn;
+					float py = cam.m_resetDirection.x * sn + cam.m_resetDirection.y * cs;
+					
+					cam.m_resetDirection = new Vector3(px, py, cam.m_resetDirection.z);
+					cam.m_resetAngle += destinationAngle;
+				}
+			}
+			
+			return;
+		}
+		
 		// ---
 		// Cheap fix. Should be a state from within CameraBehavior.cs.
 		Transform parent = transform.parent;
-
+		
 		if(parent != null && parent.tag != "WallPivot")
 			Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
 		// --- //*/
-
-        float realRotationChanged = currentRotationChange * Time.smoothDeltaTime;
-
-        if (Utilities.isBounded(
-            rotationAngles[currentIndex] - realRotationChanged,
-            rotationAngles[currentIndex] + realRotationChanged,
-            transform.eulerAngles.z))
-        {
-           transform.RotateAround(realRotationPt, Vector3.forward, rotationAngles[currentIndex] - transform.eulerAngles.z);
-            currentRotationChange = 0.0f;
-        }
-        else
-            transform.RotateAround(realRotationPt, Vector3.forward, currentRotationChange * Time.smoothDeltaTime);
-    }
+		
+		float realRotationChanged = currentRotationChange * Time.smoothDeltaTime;
+		
+		if (Utilities.isBounded(
+			rotationAngles[currentIndex] - realRotationChanged,
+			rotationAngles[currentIndex] + realRotationChanged,
+			transform.eulerAngles.z))
+		{
+			transform.RotateAround(realRotationPt, Vector3.forward, rotationAngles[currentIndex] - transform.eulerAngles.z);
+			currentRotationChange = 0.0f;
+		}
+		else
+			transform.RotateAround(realRotationPt, Vector3.forward, currentRotationChange * Time.smoothDeltaTime);
+	}
 	#endregion
-
+	
 	#region Static Methods
 	#endregion
-
+	
 	#region Utilities
 	#endregion
 }
