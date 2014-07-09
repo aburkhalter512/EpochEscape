14b1fc21cc04954c504e32dce99c90fe28013b56
Terry Rogers <terryrogers@outlook.com>
2014-07-09 08:12:57 -0700

    Began preliminary code for a level exporter (level editor) and level importer (level loading). The code consumes a bit of memory, but it's a step in the right direction.

---
+++ EpochEscape/Assets/Exporter.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * This script works, but it may consume more memory than using XML since C# isn't optimized for JSON.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class Exporter : MonoBehaviour
+{
+	void Start()
+	{
+		GameObject guards = GameObject.Find("Guards");
+
+		if(guards == null) return;
+
+		int guardCount = guards.transform.childCount;
+
+		if(guardCount == 0) return;
+
+		// Create the dictionary for the level data.
+		Dictionary<string, object> levelData = new Dictionary<string, object>();
+
+		// Create the array of dictionaries for each guard, and assign it to the key "guards".
+		levelData["guards"] = new Dictionary<string, object>[guardCount];
+
+		Transform guardTemp = null;
+		Vector3 initialPosition = Vector3.zero;
+		string guardType = null;
+		Guard guardScript = null;
+		Vector3[] patrolPoints = null;
+
+		for(int i = 0; i < guardCount; i++)
+		{
+			guardTemp = guards.transform.GetChild(i);
+			guardType = guardTemp.name;
+			initialPosition = guardTemp.position;
+			guardScript = guardTemp.GetComponent<Guard>();
+
+			// Create the dictionary for the ith guard.
+			( (Dictionary<string, object>[]) (levelData["guards"]) )[i] = new Dictionary<string, object>();
+
+			// Assign the type of the ith guard in the dictionary to the key "guardType".
+			( (Dictionary<string, object>[]) (levelData["guards"]) )[i]["guardType"] = guardType;
+
+			// Create the dictionary for the ith guard's initial location coordinates, and assign it to the key "initialLocation".
+			( (Dictionary<string, object>[]) (levelData["guards"]) )[i]["initialLocation"] = new Dictionary<string, object>();
+
+			// Store the x, y, and z coordinate of the ith guard's initial position to the respective keys.
+			( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["initialLocation"] ) )["x"] = initialPosition.x;
+			( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["initialLocation"] ) )["y"] = initialPosition.y;
+			( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["initialLocation"] ) )["z"] = initialPosition.z;
+
+			if(guardScript != null && guardType != "StationaryGuard")
+			{
+				patrolPoints = guardScript.m_patrolPoints;
+
+				// Create the array of dictionaries for the ith guard's patrol points, and assign it to the key "patrolPoints".
+				( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["patrolPoints"] = new Dictionary<string, object>[patrolPoints.Length];
+
+				for(int j = 0; j < patrolPoints.Length; j++)
+				{
+					// Create the dictionary for the ith guard's jth patrol point.
+					( (Dictionary<string, object>[]) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["patrolPoints"] ) ) [j] = new Dictionary<string, object>();
+
+					// Store the x, y, and z coordinate of the ith guard's jth patrol point to the respective keys.
+					( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["patrolPoints"] ) ) [j] ) ) ["x"] = patrolPoints[j].x;
+					( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["patrolPoints"] ) ) [j] ) ) ["y"] = patrolPoints[j].y;
+					( (Dictionary<string, object>) ( ( (Dictionary<string, object>[]) ( ( (Dictionary<string, object>[]) (levelData["guards"]) ) [i]["patrolPoints"] ) ) [j] ) ) ["z"] = patrolPoints[j].z;
+				}
+			}
+		}
+
+		// Serialize the data to create a JSON string.
+		string json = Json.Serialize(levelData);
+
+		Debug.Log(json);
+	}
+}
---
+++ EpochEscape/Assets/Importer.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * This script has a few bugs, but it works. It may consume more memory than using XML since C# isn't optimized for JSON.
+ * 
+ * Bug: For some reason, the initial position isn't set correctly despite being read properly.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class Importer : MonoBehaviour
+{
+	void Start()
+	{
+		// JSON data for level 2.
+		string levelDataJSON = "{\"guards\":[{\"guardType\":\"Guard\",\"initialLocation\":{\"x\":-1,\"y\":-2.2,\"z\":0},\"patrolPoints\":[{\"x\":-1,\"y\":-2.2,\"z\":0},{\"x\":-1,\"y\":1,\"z\":0}]},{\"guardType\":\"Guard\",\"initialLocation\":{\"x\":-2.6,\"y\":3.4,\"z\":0},\"patrolPoints\":[{\"x\":-2.6,\"y\":-3.4,\"z\":0},{\"x\":-2.6,\"y\":3.4,\"z\":0}]},{\"guardType\":\"Guard\",\"initialLocation\":{\"x\":-2,\"y\":3.9,\"z\":0},\"patrolPoints\":[{\"x\":-2,\"y\":3.9,\"z\":0},{\"x\":2.1,\"y\":3.9,\"z\":0}]},{\"guardType\":\"StationaryGuard\",\"initialLocation\":{\"x\":0.6,\"y\":0.5,\"z\":0}}]}";
+
+		// Deserialize the level data, and assign it to a dictionary.
+		Dictionary<string, object> levelData = Json.Deserialize(levelDataJSON) as Dictionary<string, object>;
+
+		// Create a list of guards.
+		List<object> guards = levelData["guards"] as List<object>;
+
+		// Temporary variables for each guard.
+		Dictionary<string, object> guard = null;
+		string guardType = null;
+		Dictionary<string, object> guardInitialLocation = null;
+		Vector3 guardLocation = Vector3.zero;
+		List<object> guardPatrolPoints = null;
+		Dictionary<string, object> guardPatrolPoint = null;
+
+		GameObject guardGameObject = null;
+		Guard guardGameObjectScript = null;
+
+		// Iterate over each guard.
+		for(int i = 0; i < guards.Count; i++)
+		{
+			// Create a dictionary for the ith guard.
+			guard = guards[i] as Dictionary<string, object>;
+
+			guardType = guard["guardType"] as string;
+
+			// Create a dictionary for the initial location.
+			guardInitialLocation = guard["initialLocation"] as Dictionary<string, object>;
+
+			// Assign the ith guard's initial location coordinates from the respective keys.
+			guardLocation.x = CastCoordinate(guardInitialLocation["x"]);
+			guardLocation.y = CastCoordinate(guardInitialLocation["y"]);
+			guardLocation.z = CastCoordinate(guardInitialLocation["z"]);
+
+			if(guardType == "Guard")
+			{
+				guardGameObject = Resources.Load("Prefabs/Enemies/Guard") as GameObject;
+				guardGameObjectScript = guardGameObject.GetComponent<Guard>();
+
+				// Create a list of patrol points from the patrolPoints key.
+				guardPatrolPoints = guard["patrolPoints"] as List<object>;
+
+				// Create the array to store the patrol points.
+				guardGameObjectScript.m_patrolPoints = new Vector3[guardPatrolPoints.Count];
+
+				// Iterate over each patrol point in the object list.
+				for(int j = 0; j < guardPatrolPoints.Count; j++)
+				{
+					// Create a dictionary for the jth patrol point.
+					guardPatrolPoint = guardPatrolPoints[j] as Dictionary<string, object>;
+
+					// Assign the patrol point from the object list to the point array.
+					guardGameObjectScript.m_patrolPoints[j].x = CastCoordinate(guardPatrolPoint["x"]);
+					guardGameObjectScript.m_patrolPoints[j].y = CastCoordinate(guardPatrolPoint["y"]);
+					guardGameObjectScript.m_patrolPoints[j].z = CastCoordinate(guardPatrolPoint["z"]);
+				}
+			}
+			else if(guardType == "StationaryGuard")
+				guardGameObject = Resources.Load("Prefabs/Enemies/StationaryGuard") as GameObject;
+
+			if(guardGameObject != null)
+				guardGameObject.transform.position = guardLocation;
+
+			Instantiate(guardGameObject);
+		}
+	}
+
+	// Casting longs and doubles to floats is acceptable since no value will ever be greater than FLOAT_MAX.
+	private float CastCoordinate(object toCast)
+	{
+		if(toCast.GetType().Name == "Int64")
+			return (float) ( (long)(toCast) );
+
+		if(toCast.GetType().Name == "Double")
+			return (float) ( (double)(toCast) );
+
+		return 0f;
+	}
+}
