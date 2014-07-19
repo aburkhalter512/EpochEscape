d031670ef76a0058ce6907e85370a33ed4abf43d
Micah Lee <micahrlee@gmail.com>
2014-07-19 00:13:52 -0700

    Fixed laser reflection

--- EpochEscape/Assets/In-progress/LaserBehavior.cs
+++ EpochEscape/Assets/In-progress/LaserBehavior.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(LineRenderer))]
 public class LaserBehavior : MonoBehaviour {
     public Transform start, end;
     public LineRenderer lr;
+    public List<Vector3> positions;
+    public Vector3 tempStart;
     int count = 2;
+
 	void Start () {
         if (lr == null)
+        {
             lr = GetComponent<LineRenderer>();
+        }
 	}
 	
 	// Update is called once per frame
@@ -18,17 +24,19 @@ public class LaserBehavior : MonoBehaviour {
 
     void RenderLaser()
     {
-        RaycastHit2D hit;
-
-        hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.up, 10000f);
-
-        lr.enabled = true;
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.up, 10000f);
+        
         lr.SetPosition(0, start.position);
+
+
         if (hit.collider != null)
         {
             lr.SetPosition(1, hit.point);
             if (hit.collider.tag == "Mirror")
             {
+                Vector2 reflectionPos = 10000 * Reflect((hit.point - new Vector2(start.position.x, start.position.y)).normalized, hit.normal);
+                lr.SetVertexCount(++count);
+                lr.SetPosition(count - 1, reflectionPos);
                 //reflect it here
             }
 
@@ -37,7 +45,12 @@ public class LaserBehavior : MonoBehaviour {
         {
             lr.SetPosition(1, end.position);
         }
-        
-        Debug.DrawLine(start.position, hit.point, Color.red);
+        //Debug.DrawLine(start.position, hit.point, Color.red);
     }
+
+    private Vector2 Reflect(Vector2 vector, Vector2 normal)
+    {
+        return vector - 2 * Vector2.Dot(vector, normal) * normal;
+    }
+
 }
