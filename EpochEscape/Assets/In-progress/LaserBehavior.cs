aa125b4fc30a2feeb51d37129d51998bc9477939
micahrlee <micahrlee@gmail.com>
2014-08-01 16:22:26 -0700

    Added Sensors

--- EpochEscape/Assets/In-progress/LaserBehavior.cs
+++ EpochEscape/Assets/In-progress/LaserBehavior.cs
@@ -2,96 +2,103 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(LineRenderer))]
-public class LaserBehavior : MonoBehaviour {
-    public Transform start, end;
-    public LineRenderer lr;
-    public List<Vector3> positions;
-    public Vector3 tempStart;
-    private int bounces = 0;
+[RequireComponent (typeof(LineRenderer))]
+public class LaserBehavior : MonoBehaviour
+{
+	public Transform start, end;
+	public LineRenderer lr;
+	public List<Vector3> positions;
+	public GameObject sensor;
+	private int bounces = 0;
 
-	void Start () {
+	void Start ()
+	{
 		positions = new List<Vector3> ();
-        if (lr == null)
-        {
-            lr = GetComponent<LineRenderer>();
-        }
+		if (lr == null) {
+			lr = GetComponent<LineRenderer> ();
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        bounces = 0;
-        BuildLaser(transform.position);
-        DrawLaser();
-        positions.Clear();
+	void Update ()
+	{
+		bounces = 0;
+		BuildLaser (transform.position);
+		DrawLaser ();
+		positions.Clear ();
 	}
 
-//    void BuildLaser(Vector3 origin, int bounces)
-//    {
-//        if (bounces > 20)
-//        {
-//            return;
-//        }
-//
-//		RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), (end.position - origin).normalized, 10000f);
-//        Vector3 reflectionPos = origin.normalized * 10000;
-//
-//        if (hit.collider == null)
-//        {
-//            positions.Add(end.position);
-//            return;
-//        }
-//
-//        //if hit mirror
-//        else if (hit.collider != null)
-//        {
-//            //add hit point (where to stop the laser)
-//            positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
-//            //if object is mirror
-//            if (hit.collider.tag == "Mirror")
-//            {
-//                //reflect against the mirror
-//                reflectionPos = 10000 * Vector3.Reflect((new Vector3(hit.point.x, hit.point.y, 0) - origin), hit.normal) + new Vector3(hit.point.x, hit.point.y, 0);
-//                positions.Add(reflectionPos);
-//            }
-//        }
-//
-//        BuildLaser(hit.point, bounces + 1);
-//        //Debug.DrawLine(start.position, hit.point, Color.red);
-//    }
+	//    void BuildLaser(Vector3 origin, int bounces)
+	//    {
+	//        if (bounces > 20)
+	//        {
+	//            return;
+	//        }
+	//
+	//		RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), (end.position - origin).normalized, 10000f);
+	//        Vector3 reflectionPos = origin.normalized * 10000;
+	//
+	//        if (hit.collider == null)
+	//        {
+	//            positions.Add(end.position);
+	//            return;
+	//        }
+	//
+	//        //if hit mirror
+	//        else if (hit.collider != null)
+	//        {
+	//            //add hit point (where to stop the laser)
+	//            positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
+	//            //if object is mirror
+	//            if (hit.collider.tag == "Mirror")
+	//            {
+	//                //reflect against the mirror
+	//                reflectionPos = 10000 * Vector3.Reflect((new Vector3(hit.point.x, hit.point.y, 0) - origin), hit.normal) + new Vector3(hit.point.x, hit.point.y, 0);
+	//                positions.Add(reflectionPos);
+	//            }
+	//        }
+	//
+	//        BuildLaser(hit.point, bounces + 1);
+	//        //Debug.DrawLine(start.position, hit.point, Color.red);
+	//    }
 
-	void BuildLaser(Vector3 origin)
+	void BuildLaser (Vector3 origin)
 	{
-        positions.Add(origin);
+		Vector3 endPos = end.position;
+		positions.Add (origin);
 		Vector3 reflectionPos = origin.normalized;
-        Vector3 endPos = end.position;
-		while (true)
-		{
-			if(bounces > 20){
+		while (true) {
+			if (bounces > 20) {
 				break;
 			}
-			RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), (endPos - origin).normalized, 100);
-			if (hit.collider == null)
-			{
-                positions.Add(endPos);
+			RaycastHit2D hit = Physics2D.Raycast (new Vector2 (origin.x, origin.y), (endPos - origin).normalized, 100);
+			if (hit.collider == null) {
+				positions.Add (endPos);
 				break;
 			}
 			//if hit something
-			else{
+			else {
 				//if object is mirror
-                positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
-				if (hit.collider.tag == "Mirror")
-				{
+				positions.Add (new Vector3 (hit.point.x, hit.point.y, 0));
+				if (hit.collider.tag == "Mirror") {
 					//reflect against the mirror
-                    reflectionPos = 100 * Vector3.Reflect((new Vector3(hit.point.x, hit.point.y, 0) - origin), hit.normal) + new Vector3(hit.point.x, hit.point.y, 0);
-                    //positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
+					reflectionPos = 100 * Vector3.Reflect ((new Vector3 (hit.point.x, hit.point.y, 0) - origin), hit.normal) + new Vector3 (hit.point.x, hit.point.y, 0);
+					//positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
 					endPos = reflectionPos;
 					origin = hit.point;
 					bounces++;
-				}
-				else{
-                    
-                    break;
+				} 
+				//if not mirror
+				else {
+					//if sensor is not null, then continue with sensor stuff
+					if (sensor != null) {
+						//if has a sensor, and is not hitting sensor or mirror
+						if (hit.collider.tag != "Sensor") {
+							//set alarm
+							sensor.GetComponent<SensorBehavior> ().Alarm ();
+						}
+					}
+					break;
 				}
 			}
 		}
@@ -100,16 +107,14 @@ public class LaserBehavior : MonoBehaviour {
 		//Debug.DrawLine(start.position, hit.point, Color.red);
 	}
 
-    void DrawLaser()
-    {
-        lr.SetWidth(.075f, .075f);
-        if (positions != null || lr != null)
-        {
-            lr.SetVertexCount(positions.Count);
-            for (int i = 0; i < positions.Count; i++)
-            {
-                lr.SetPosition(i, positions[i]);
-            }
-        }
-    }
+	void DrawLaser ()
+	{
+		lr.SetWidth (.075f, .075f);
+		if (positions != null || lr != null) {
+			lr.SetVertexCount (positions.Count);
+			for (int i = 0; i < positions.Count; i++) {
+				lr.SetPosition (i, positions [i]);
+			}
+		}
+	}
 }
