e8c78958f7669402e592f573721eef172ff46386
micahrlee <micahrlee@gmail.com>
2014-08-05 02:25:53 -0700

    Added collectors, new line draw, and udpated how lasers work.

--- EpochEscape/Assets/In-progress/LaserBehavior.cs
+++ EpochEscape/Assets/In-progress/LaserBehavior.cs
@@ -1,68 +1,35 @@
 ﻿using UnityEngine;
+using Vectrosity;
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent (typeof(LineRenderer))]
 public class LaserBehavior : MonoBehaviour
 {
 	public Transform start, end;
-	public LineRenderer lr;
+	public float lineWidth;
+	public VectorLine vLine;
+	public Color color;
 	public List<Vector3> positions;
 	public GameObject sensor;
-	private int bounces = 0;
-
+	protected int bounces = 0;
+	
 	void Start ()
 	{
 		positions = new List<Vector3> ();
-		if (lr == null) {
-			lr = GetComponent<LineRenderer> ();
-		}
 	}
-	
+
+	void Update(){
+	}
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
 	{
 		bounces = 0;
-		BuildLaser (transform.position);
+		BuildLaser (start.position);
 		DrawLaser ();
 		positions.Clear ();
 	}
-
-	//    void BuildLaser(Vector3 origin, int bounces)
-	//    {
-	//        if (bounces > 20)
-	//        {
-	//            return;
-	//        }
-	//
-	//		RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), (end.position - origin).normalized, 10000f);
-	//        Vector3 reflectionPos = origin.normalized * 10000;
-	//
-	//        if (hit.collider == null)
-	//        {
-	//            positions.Add(end.position);
-	//            return;
-	//        }
-	//
-	//        //if hit mirror
-	//        else if (hit.collider != null)
-	//        {
-	//            //add hit point (where to stop the laser)
-	//            positions.Add(new Vector3(hit.point.x, hit.point.y, 0));
-	//            //if object is mirror
-	//            if (hit.collider.tag == "Mirror")
-	//            {
-	//                //reflect against the mirror
-	//                reflectionPos = 10000 * Vector3.Reflect((new Vector3(hit.point.x, hit.point.y, 0) - origin), hit.normal) + new Vector3(hit.point.x, hit.point.y, 0);
-	//                positions.Add(reflectionPos);
-	//            }
-	//        }
-	//
-	//        BuildLaser(hit.point, bounces + 1);
-	//        //Debug.DrawLine(start.position, hit.point, Color.red);
-	//    }
-
-	void BuildLaser (Vector3 origin)
+	
+	public void BuildLaser (Vector3 origin)
 	{
 		Vector3 endPos = end.position;
 		positions.Add (origin);
@@ -98,23 +65,26 @@ public class LaserBehavior : MonoBehaviour
 							sensor.GetComponent<SensorBehavior> ().Alarm ();
 						}
 					}
+
+					if(hit.collider.tag == "Collector"){
+						CollectorColors c = hit.collider.gameObject.GetComponent <CollectorColors>();
+						c.color = color;
+						c.Collect();
+					}
 					break;
 				}
 			}
 		}
-		
-		//BuildLaser(hit.point, bounces + 1);
-		//Debug.DrawLine(start.position, hit.point, Color.red);
+	}
+	
+	public void DrawLaser ()
+	{
+		vLine = VectorLine.SetLine (color, .1f, positions.ToArray ());
+		vLine.lineWidth = lineWidth;
+		vLine.Draw ();
 	}
 
-	void DrawLaser ()
-	{
-		lr.SetWidth (.075f, .075f);
-		if (positions != null || lr != null) {
-			lr.SetVertexCount (positions.Count);
-			for (int i = 0; i < positions.Count; i++) {
-				lr.SetPosition (i, positions [i]);
-			}
-		}
+	public void SetColor(Color c){
+		color = c;
 	}
 }
