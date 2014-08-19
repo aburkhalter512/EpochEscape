41621e51c5afce0b589bd80d5233037f930093e7
Terry Rogers <terryrogers@outlook.com>
2014-08-19 14:31:18 -0700

    Resized the art asset of each level's floor to operate at 32px by 32px tiles instead of the 100px by 100px. Doing so allows for better compression. Even though the art assets are smaller, it's nearly impossible to notice since the camera is now at a size of 3. Performing this texture optimization cut ~340MB off of the build size.

---
+++ EpochEscape/Assets/FloorUnits.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorUnits : MonoBehaviour
+{
+	public void Start()
+	{
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+		if(sr != null)
+		{
+			Debug.Log(sr.sprite.bounds.size.x);
+		}
+	}
+}
