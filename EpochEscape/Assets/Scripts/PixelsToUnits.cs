e7948dd97d9d9e2d28f21823e0daf524321e4afe
Terry Rogers <terryrogers@outlook.com>
2014-08-19 23:02:29 -0700

    Various tweaks.

---
+++ EpochEscape/Assets/Scripts/PixelsToUnits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelsToUnits : MonoBehaviour
+{
+	public void Start()
+	{
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+		if(sr != null)
+			Debug.Log(sr.sprite.bounds.size.x);
+	}
+}
