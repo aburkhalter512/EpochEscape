76f0ee31eb991c426b229018068cc3aa5a2b16c8
Terry Rogers <terryrogers@outlook.com>
2014-09-02 17:18:57 -0700

    Tabs/spaces change.

--- EpochEscape/Assets/In-progress/Michael/HoldableBox.cs
+++ EpochEscape/Assets/In-progress/Michael/HoldableBox.cs
@@ -2,66 +2,66 @@
 using System.Collections;
 
 public class HoldableBox : InteractiveObject {
-	protected enum direction {
-		Left, Right, Up, Down, Static
-	};
+    protected enum direction {
+        Left, Right, Up, Down, Static
+    };
 
-	//public float k_speed = 0.05f;
-	protected bool m_isInUse = false;
-	protected direction m_direction;
-	protected Player p = null;
-	
-	public void Start () {
-		m_direction = direction.Static;
-		//GameObject g = GameObject.FindGameObjectWithTag("Player");
-		//p = g.GetComponent<Player>();
-	}
+    //public float k_speed = 0.05f;
+    protected bool m_isInUse = false;
+    protected direction m_direction;
+    protected Player p = null;
+    
+    public void Start () {
+        m_direction = direction.Static;
+        //GameObject g = GameObject.FindGameObjectWithTag("Player");
+        //p = g.GetComponent<Player>();
+    }
 
-	public void Update () {
-		transform.localScale = new Vector3(1f,1f,1f);
+    public void Update () {
+        transform.localScale = new Vector3(1f,1f,1f);
 
-		if(p == null)
-			FindPlayer();
-	}
+        if(p == null)
+            FindPlayer();
+    }
 
-	public override void Interact() {
-		Debug.Log ("Box Interact");
-		//renderer.enabled = false;
-		collider2D.enabled = false;
-		m_isInUse = true;
+    public override void Interact() {
+        Debug.Log ("Box Interact");
+        //renderer.enabled = false;
+        collider2D.enabled = false;
+        m_isInUse = true;
 
-		if(p != null)
-		{
-			p.m_isHoldingBox = true;
-			transform.parent = p.transform;
-		}
-	}
-	
-	public void Place() { //places the box in front of the player
-		//transform.position = p.transform.position;
-		transform.localScale = new Vector3(1f,1f,1f);
-		transform.rotation = Quaternion.identity;
-		renderer.enabled = true;
-		collider2D.enabled = true;
-		transform.parent = null;
-		m_isInUse = false;
+        if(p != null)
+        {
+            p.m_isHoldingBox = true;
+            transform.parent = p.transform;
+        }
+    }
+    
+    public void Place() { //places the box in front of the player
+        //transform.position = p.transform.position;
+        transform.localScale = new Vector3(1f,1f,1f);
+        transform.rotation = Quaternion.identity;
+        renderer.enabled = true;
+        collider2D.enabled = true;
+        transform.parent = null;
+        m_isInUse = false;
 
-		if(p != null)
-			p.m_isHoldingBox = false;
-	}
-	
-	public void Die() {
-		Destroy(gameObject);
-	}
+        if(p != null)
+            p.m_isHoldingBox = false;
+    }
+    
+    public void Die() {
+        Destroy(gameObject);
+    }
 
-	private void FindPlayer()
-	{
-		if(p == null)
-		{
-			GameObject player = GameObject.FindWithTag("Player");
-			
-			if(player != null)
-				p = player.GetComponent<Player>();
-		}
-	}
+    private void FindPlayer()
+    {
+        if(p == null)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            
+            if(player != null)
+                p = player.GetComponent<Player>();
+        }
+    }
 }
