df764f129d36680a5eb73b4dbbc38d3b2fe23a9f
aburkhalter512 <aburkhalter512@gmail.com>
2015-02-02 12:54:53 -0800

    Refactored the TimerDoor to avoid tight coupling. The HUDManager now manages displaying the timer label instead of the TimerDoor itself.

--- EpochEscape/Assets/Scripts/Activatables/Doors/Frames/TimerDoorFrame.cs
+++ EpochEscape/Assets/Scripts/Activatables/Doors/Frames/TimerDoorFrame.cs
@@ -5,85 +5,56 @@ public class TimerDoorFrame : StandardDoorFrame
 {
     #region Interface Variables
     public int time = 1;
-
-    public GUIStyle timerStyle;
     #endregion
 
     #region Instance Variables
-    private int timeRemaining = 0;
-    public TimerState m_timerState = TimerState.Ready;
+    private int mTimeRemaining = 0;
+    private bool mIsTiming = false;
     #endregion
 
-    #region Class Constants
-    public enum TimerState
+    protected new void Start()
     {
-        Ready,
-        Paused,
-        Running,
-        Expired,
-        Beat
-    };
-    #endregion
+        initialState = STATE.INACTIVE;
+
+        base.Start();
+    }
 
     #region Interface Methods
-    public override void triggerFrontEnter()
+    public override void toggle()
     {
-        m_timerState = TimerState.Beat;
+        if (mState == STATE.INACTIVE)
+            activate();
+    }
+
+    public override void activate()
+    {
+        StartCoroutine(countDownTimer());
+    }
+
+    public int getTimeRemaining()
+    {
+        return mTimeRemaining;
+    }
+
+    public bool isTiming()
+    {
+        return mIsTiming;
     }
     #endregion
 
     #region Instance Methods
-    protected IEnumerator startTimer()
-    {
-        m_timerState = TimerState.Running;
-
-        yield return StartCoroutine(countDownTimer());
-
-        timerStyle.normal.textColor = Color.white;
-    }
-
     private IEnumerator countDownTimer()
     {
-        int panicTime = 5;
-        bool panic = false;
-        Color panicColor = Color.red;
+        base.activate();
+        mIsTiming = true;
+        HUDManager.SetTimer(this);
 
-        for (timeRemaining = time; timeRemaining >= 0; timeRemaining--)
-        {
-            if(m_timerState == TimerState.Beat)
-                yield break;
-
-            if(timeRemaining <= panicTime && !panic)
-            {
-                panic = true;
-
-                timerStyle.normal.textColor = panicColor;
-            }
-
+        for (mTimeRemaining = time; mTimeRemaining >= 0; mTimeRemaining--)
             yield return new WaitForSeconds(1f);
-        }
 
-        m_timerState = TimerState.Expired;
-        timeRemaining = 0;
-
-        if (m_timerState != TimerState.Beat && timeRemaining == 0)
-        {
-            if (m_timerState == TimerState.Ready)
-            {
-                timeRemaining = time;
-
-                m_timerState = TimerState.Paused;
-            }
-            else if (m_timerState == TimerState.Running)
-                timeRemaining = time;
-        }
+        deactivate();
+        mTimeRemaining = 0;
+        mIsTiming = false;
     }
     #endregion
-    
-    protected void OnGUI()
-    {
-    	if(m_timerState == TimerState.Paused || m_timerState == TimerState.Running) {
-			GUI.Label(new Rect(Screen.width / 2 - 50f, 30f, 100f, 100f), timeRemaining.ToString(), timerStyle);
-    	}
-    }
 }
