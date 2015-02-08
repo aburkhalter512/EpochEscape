b6e89ffb583332cd3fc68775ee3c3deacc350012
aburkhalter512 <aburkhalter512@gmail.com>
2015-02-07 16:48:38 -0800

    Created the item factory to process and generate items. It currently only supports power cores.

---
+++ EpochEscape/Assets/Scripts/Game Environment/ItemFactory.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Xml;
+using System;
+using System.Collections.Generic;
+
+public class ItemFactory : Factory<MonoBehaviour>
+{
+	#region Interface Methods
+    public override MonoBehaviour create(XmlElement element)
+    {
+        if (element == null || element.Name != "item")
+            return null;
+
+        MonoBehaviour retVal = null;
+
+        switch (element.GetAttribute("type"))
+        {
+            case "powercore":
+                retVal = createBasicPowerCore(element);
+                break;
+        }
+
+        return retVal;
+    }
+	#endregion
+	
+	#region Instance Methods
+    private MonoBehaviour createBasicPowerCore(XmlElement element)
+    {
+        GameObject go = Resources.Load<GameObject>("Prefabs/Game Environment/Items/PowerCore");
+        PowerCore retVal = go.GetComponent<PowerCore>();
+
+        deserializeComponents(go, element);
+
+        return retVal;
+    }
+
+    private static void deserializeComponents(GameObject go, XmlElement parent)
+    {
+        XmlNode child = parent.FirstChild;
+        XmlElement component;
+
+        while (child != null)
+        {
+            component = child as XmlElement;
+            if (component != null)
+            {
+                switch (component.Name)
+                {
+                    case "transform":
+                        ComponentSerializer.deserialize(go.transform, component);
+                        break;
+                    case "spriterenderer":
+                        ComponentSerializer.deserialize(go.GetComponent<SpriteRenderer>(), component);
+                        break;
+                }
+            }
+
+            child = child.NextSibling;
+        }
+    }
+	#endregion
+}
